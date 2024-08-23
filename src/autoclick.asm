e7b625025b1303e1ffeaf5ab8436898485df4724
Donotavio <ribeitemp@gmail.com>
2024-08-23 11:39:36 -0300

    feat(assembly): implement auto clicker loop with configurable interval and click limit
    - Added interval and numClicks data sections to set the delay between clicks and the maximum number of clicks.
    - Implemented the main click loop with a conditional exit after reaching the specified number of clicks.
    - Added message display upon completion using MessageBoxA.
    - Integrated mouse click routine and sleep function to manage the timing of clicks.
    
    Author: Otávio Ribeiro <ribeitemp@gmail.com>

--- src/autoclick.asm
+++ src/autoclick.asm
@@ -1,36 +1,72 @@
 section .data
-    clickInterval dd 1000          ; Intervalo entre cliques (1000ms = 1 segundo)
-    mouse_event db 0x20, 0x00, 0x00, 0x00, 0x00
-    leftDown dd 0x02               ; Constante para o clique do botão esquerdo (down)
-    leftUp dd 0x04                 ; Constante para o clique do botão esquerdo (up)
-    exitMsg db 'Auto clicker finished', 0
+    interval dd 1000                 ; Intervalo entre cliques em milissegundos (1 segundo)
+    numClicks dd 100                 ; Número de cliques antes de parar
+    clickCount dd 0                  ; Contador de cliques
+    msg db 'Auto clicker finished.', 0
+
+section .bss
+    hInstance resd 1
+    hWnd resd 1
 
 section .text
     global _start
+    extern Sleep, MessageBoxA, ExitProcess, GetModuleHandleA
 
 _start:
-    ; Loop de auto click
-    click_loop:
-        ; Chamada para o clique do botão esquerdo (down)
-        mov eax, 0x07              ; Código da função de sistema 'Sleep'
-        mov ebx, leftDown          ; Argumento para a função de clique (botão down)
-        int 0x80                   ; Interrupção de sistema
+    ; Inicializa contadores e parâmetros
+    mov eax, dword [interval]
+    mov ebx, dword [numClicks]
 
-        ; Chamada para o clique do botão esquerdo (up)
-        mov eax, 0x07              ; Código da função de sistema 'Sleep'
-        mov ebx, leftUp            ; Argumento para a função de clique (botão up)
-        int 0x80                   ; Interrupção de sistema
+click_loop:
+    ; Realiza clique do botão esquerdo
+    call MouseClick
 
-        ; Espera pelo intervalo antes do próximo clique
-        mov eax, 0x07              ; Código da função de sistema 'Sleep'
-        mov ebx, clickInterval     ; Passa o intervalo de espera em milissegundos
-        int 0x80                   ; Interrupção de sistema
+    ; Incrementa o contador de cliques
+    inc dword [clickCount]
 
-        jmp click_loop             ; Repete o loop de clique
+    ; Verifica se o número de cliques atingiu o máximo
+    mov eax, dword [clickCount]
+    cmp eax, ebx
+    jge end_click
 
-    ; Código para terminar o programa
-    exit:
-        mov eax, 1                 ; Código para função de saída do programa
-        xor ebx, ebx               ; Código de saída 0 (sucesso)
-        int 0x80                   ; Interrupção de sistema para sair
+    ; Espera pelo intervalo antes do próximo clique
+    push dword [interval]
+    call Sleep
 
+    ; Volta para o loop de clique
+    jmp click_loop
+
+end_click:
+    ; Exibe mensagem de conclusão
+    push 0
+    push msg
+    push 0
+    push 0
+    call MessageBoxA
+
+    ; Termina o programa
+    push 0
+    call ExitProcess
+
+; Rotina para realizar clique do mouse
+MouseClick:
+    ; Usa a função de chamada de sistema para o clique
+    ; (botão esquerdo: down e up)
+    push 0x04
+    push 0x02
+    push 0
+    push 0
+    call mouse_event
+    ret
+
+section .idata
+    dd 0, 0, 0, 0, RVA kernel32_dll_name
+    dd 0, 0, 0, 0, RVA user32_dll_name
+    dd 0, 0, 0, 0, 0
+
+kernel32_dll_name db 'KERNEL32.DLL', 0
+user32_dll_name db 'USER32.DLL', 0
+
+section .rdata
+    dd 0, 0, 0, 0, 0
+    dd 0, 0, 0, 0, 0
